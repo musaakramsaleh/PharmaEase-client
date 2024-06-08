15d0add85112446b83b2921d3c3262d13c96c6d3
musaakramsaleh <saleh01715972188@gmail.com>
2024-06-08 17:44:38 +0600

    ok

--- src/main.jsx
+++ src/main.jsx
@@ -4,10 +4,13 @@ import './index.css'
 import { RouterProvider } from 'react-router-dom'
 import router from './Routes/Routes'
 import Firebase_Provider from './Component/Firebase-provider/Firebase_Provider'
+import { QueryClient,QueryClientProvider} from '@tanstack/react-query'
 
+const queryClient = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  
+    
+    <QueryClientProvider client={queryClient}>
     <Firebase_Provider>
       <React.StrictMode>
    <div className='max-w-[1440px] mx-auto'>
@@ -15,5 +18,6 @@ ReactDOM.createRoot(document.getElementById('root')).render(
    </div>
   </React.StrictMode>
     </Firebase_Provider>
+    </QueryClientProvider>
   
 )
