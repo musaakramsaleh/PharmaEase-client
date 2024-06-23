9865294bc69667ad8f3f5be1772a0b67c5e24ee2
musaakramsaleh <saleh01715972188@gmail.com>
2024-06-23 09:29:14 +0600

    ok

--- src/Home/Partner.jsx
+++ src/Home/Partner.jsx
@@ -11,19 +11,19 @@ const Partner = () => {
            <Headline title="Our Partners"></Headline>
            <div className='max-w-[1440px] mx-auto grid grid-cols-1 md:grid-cols-4 gap-3 mt-10 place-items-center mb-16'>
            <div>
-            <img src={johnson} alt="" />
+            <img className='h-[200px]' src={johnson} alt="" />
             <p className='text-center text-2xl font-bold'>Johnson and Johnson</p>
            </div>
            <div>
-            <img src={Pfyzer} alt="" />
+            <img className='h-[200px]' src={Pfyzer} alt="" />
             <p className='text-center text-2xl font-bold'>Pfizer</p>
            </div>
            <div>
-            <img src={Moderna} alt="" />
+            <img className='h-[200px]' src={Moderna} alt="" />
             <p className='text-center text-2xl font-bold'>Moderna</p>
            </div>
            <div>
-            <img src={Bayer} alt="" />
+            <img className='h-[200px]' src={Bayer} alt="" />
             <p className='text-center text-2xl font-bold'>Bayer</p>
            </div>
            </div>
